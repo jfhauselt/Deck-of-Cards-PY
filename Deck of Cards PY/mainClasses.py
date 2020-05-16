4d31470c2b14a8b42783bb853e0aafdd4fe5262e
John F Hauselt <john.hauselt@uconn.edu>
2020-05-16 10:37:42 -0400

    split into different classes

---
+++ Deck of Cards PY/mainClasses.py
@@ -0,0 +1,39 @@
+import random
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return self.suit + " " + self.rank
+
+class Deck:
+    def __init__(self):
+        self.deckorder = []
+    def add(self,card):
+        self.deckorder.append(card)
+    def shuffle(self):
+        random.shuffle(self.deckorder)
+    def draw(self):
+        return self.deckorder.pop()
+    def __iter__(self):
+        return self.deckorder.__iter__()
+
+#utilities
+
+#creates standard 52 card deck
+def createDeck():
+    Suits = ["hearts","clubs","diamonds","spades"]
+    Ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    tempDeck = Deck()
+    for i in Suits:
+        for j in Ranks:
+            tempDeck.add(Card(i,j))
+    return tempDeck
+
+#takes a standard deck and creates a standard 5 card hand
+
+def createHand(mydeck: Deck):
+    tempHand = Deck()
+    for i in range(5):
+        tempHand.add(mydeck.draw())
+    return tempHand
---
+++ Deck of Cards PY/testCase.py
@@ -0,0 +1,14 @@
+import mainClasses
+
+def testShuffle():
+    for i in range(1000):
+        mydeck = mainClasses.createDeck()
+        mydeck.shuffle()
+    print("created 1000 decks")
+
+def testDraw():
+    mydeck = mainClasses.createDeck()
+    myHand = mainClasses.createHand(mydeck)
+    for i in myHand:
+        print(i)
+
